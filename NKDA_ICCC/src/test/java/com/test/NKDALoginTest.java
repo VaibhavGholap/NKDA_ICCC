723a2b71cb95678284e1255b8462d1ac0587f450
VaibhavGholap <vaibhav.gholap5@gmail.com>
2024-05-17 12:20:52 +0530

    Changes for Explicit Wait

--- NKDA_ICCC/src/test/java/com/test/NKDALoginTest.java
+++ NKDA_ICCC/src/test/java/com/test/NKDALoginTest.java
@@ -17,9 +17,11 @@ public class NKDALoginTest extends BaseClass {
 	}
 	@Test
 	public void loginTest() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("testsite@yopmail.com");
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys("Vaibhav@123");
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//button[contains(@class,'btn btn-primary')]")).click();
 		Thread.sleep(2000);
         String actualTitle=driver.getTitle();
